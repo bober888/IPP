51d0deb69df61812adf982eb9b7782b7b076714d
Yehor Pohrebniak <bobermister@gmail.com>
2021-02-23 22:43:13 +0100

    60% ready

--- parse.php
+++ parse.php
@@ -17,19 +17,20 @@ abstract class Errors {
     const INVALID_ARGUMENTS = 10;
     const ERROR_CODE_HEAD = 21;
     const ERROR_OPCODE = 22; 
-    const _ERROR_LEXICAL_SYNT = 23; 
+    const ERROR_LEXICAL_SYNT = 23; 
 }
 
 class patterns {
     private const
         comment = "\s*(#(.)*)?$/",
-        tbool = "bool@(true|false)",
-        tnil = "nil@nil",
-        tint = "int@\-?[0-9]+",
-        tstring = "string@(|(\\\\\d{3})|[^#\\\\\"\s])+",
+        tbool = "\s+bool@(true|false)",
+        tnil = "\s+nil@nil",
+        tint = "\s+int@\-?[0-9]+",
+        tstring = "\s+string@(|(\\\\\d{3})|[^#\\\\\"\s])+",
         var1 = "\s+(GF|TF|LF)@(\_|\?|\-|\\$|\&|\%|\\*|\!|[A-z])+(\_|\?|\-|\\$|\&|\%|\\*|\!|[A-z]|[0-9])*",
-        symb = "\s+((" . self::var1 . ")|(" . self::tbool . ")|(" . self::tnil . ")|(" . self::tstring . ")|(" . self::tint . "))",
-        label = "\s+(\_|\?|\-|\\$|\&|\%|\\*|\!|[A-z])+(\_|\?|\-|\\$|\&|\%|\\*|\!|[A-z]|[0-9])*";
+        symb = "((" . self::var1 . ")|(" . self::tbool . ")|(" . self::tnil . ")|(" . self::tstring . ")|(" . self::tint . "))",
+        label = "\s+(\_|\?|\-|\\$|\&|\%|\\*|\!|[A-z])+(\_|\?|\-|\\$|\&|\%|\\*|\!|[A-z]|[0-9])*",
+        type = "\s+(int|string|bool)";
 
     private const instructions = [
         "(?i)MOVE(?-i)" => ["var", "symb"],
@@ -39,14 +40,48 @@ class patterns {
         "(?i)DEFVAR(?-i)" => ["var"],
         "(?i)CALL(?-i)" => ["label"],
         "(?i)RETURN(?-i)" => [],
+        "(?i)PUSHS(?-i)" => ["symb"],
+        "(?i)POPS(?-i)" => ["var"],
+        "(?i)ADD(?-i)" => ["var", "symb", "symb"],
+        "(?i)SUB(?-i)" => ["var", "symb", "symb"],
+        "(?i)MUL(?-i)" => ["var", "symb", "symb"],
+        "(?i)IDIV(?-i)" => ["var", "symb", "symb"],
+        "(?i)LT(?-i)" => ["var", "symb", "symb"],
+        "(?i)EQ(?-i)" => ["var", "symb", "symb"],
+        "(?i)GT(?-i)" => ["var", "symb", "symb"],
+        "(?i)AND(?-i)" => ["var", "symb", "symb"],
+        "(?i)OR(?-i)" => ["var", "symb", "symb"],
+        "(?i)NOT(?-i)" => ["var", "symb", "symb"],
+        "(?i)INT2CHAR(?-i)" => ["var", "symb"],
+        "(?i)STRI2INT(?-i)" => ["var", "symb", "symb"],
+        "(?i)READ(?-i)" => ["var", "type"],
+        "(?i)WRITE(?-i)" => ["symb"],
+        "(?i)CONCAT(?-i)" => ["var", "symb", "symb"],
+        "(?i)STRLEN(?-i)" => ["var", "symb"],
+        "(?i)GETCHAR(?-i)" => ["var", "symb", "symb"],
+        "(?i)SETCHAR(?-i)" => ["var", "symb", "symb"],
+        "(?i)TYPE(?-i)" => ["var", "symb"],
+        "(?i)LABEL(?-i)" => ["label"],
+        "(?i)JUMP(?-i)" => ["label"],
+        "(?i)JUMPIFEQ(?-i)" => ["label", "symb", "symb"],
+        "(?i)JUMPIFNEQ(?-i)" => ["label", "symb", "symb"],
+        "(?i)EXIT(?-i)" => ["symb"],
+        "(?i)DPRINT(?-i)" => ["symb"],
+        "(?i)BREAK(?-i)" => [],
     ];
 
-    public function parser($string) {
-        
+    public static function parser($string) {
+        $ErrorInstrFlag = true; //Flag for invalid instruction error
+
         foreach(self::instructions as $instruction => $oprts) {
             $pattern = "";
             $pattern = "/^" . $instruction;
+            $patternInstr = $pattern . "/";
             
+            if(preg_match($patternInstr, $string)){
+                $ErrorInstrFlag = false;
+            }
+
             foreach($oprts as $oprt) {
                 switch($oprt){
                     case "var":
@@ -58,18 +93,26 @@ class patterns {
                     case "label":
                         $pattern .= self::label;
                         break;
+                    case "type":
+                        $pattern .= self::type;
                     default:
                         break;
                 }
             }
 
             $pattern .= self::comment;
-    
+            
             if(preg_match($pattern, $string)) {
-                return true;
+                return 0;
             }
+
         }
-        return false;
+
+        if($ErrorInstrFlag) {
+            return Errors::ERROR_OPCODE;
+        } else {
+            return Errors::ERROR_LEXICAL_SYNT;
+        }
     }
 }
 
@@ -105,15 +148,26 @@ while ($c = fgets(STDIN)) {
             $headerFlag = true;
             $str = "";
             continue;
+        } else if (!$headerFlag && preg_match("/^\s*#(.)*$/", $str)) { //if first line is comment
+            $str = "";
+            continue;
         } else if (!$headerFlag) {
             exit(Errors::ERROR_CODE_HEAD);
         }
 
+        //If line is command
+        if (preg_match("/^\s*#(.)*$/", $str)) {
+            echo "comment\n";
+            $str = "";
+            continue;    
+        }
         
-        if(patterns::parser($str)){
+        //Check if string correct, if not program exit
+        if(patterns::parser($str) == 0) {
             echo "line succ\n";
         } else {
-            echo "line notsucc\n", $str;
+            print $str;
+            exit(patterns::parser($str));
         }
         //Clear string
         $str = "";
